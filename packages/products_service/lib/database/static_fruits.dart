9e9232c40b552f5106583f89cbc76a2abdd39269
Jan Stepien <jasiek.stepien@gmail.com>
2022-09-07 11:45:41 +0200

    fix: add price to database

--- packages/products_service/lib/database/static_fruits.dart
+++ packages/products_service/lib/database/static_fruits.dart
@@ -5,7 +5,8 @@ const staticFruit = {
   'id': 1,
   'assetImageFile': 'apple.jpg',
   'productName': 'Apple',
-  'productDescription': 'Vitamins, Minerals very high number,'
+  'productDescription': 'Vitamins, Minerals very high number,',
+  'price': 1.99
 };
 
 /// {@template static_fruits}
@@ -17,66 +18,77 @@ const staticFruits = [
     'id': 2,
     'assetImageFile': 'blueberry.jpg',
     'productName': 'Blueberry',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 2.99
   },
   {
     'id': 3,
     'assetImageFile': 'grapes.jpg',
     'productName': 'Grapes',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 3.99
   },
   {
     'id': 4,
     'assetImageFile': 'green_grapefruit.jpg',
     'productName': 'Green grapefruit',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 4.99
   },
   {
     'id': 5,
     'assetImageFile': 'lemon.jpg',
     'productName': 'Lemon',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 5.99
   },
   {
     'id': 6,
     'assetImageFile': 'mango.jpg',
     'productName': 'Mango',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 6.99
   },
   {
     'id': 7,
     'assetImageFile': 'orange.jpg',
     'productName': 'Orange',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 7.99
   },
   {
     'id': 8,
     'assetImageFile': 'pear.jpg',
     'productName': 'Pear',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 8.99
   },
   {
     'id': 9,
     'assetImageFile': 'pineapple.jpg',
     'productName': 'Pineapple',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 9.99
   },
   {
     'id': 10,
     'assetImageFile': 'red_grapefruit.jpg',
     'productName': 'Red grapefruit',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 10.99
   },
   {
     'id': 11,
     'assetImageFile': 'tangerine.jpg',
     'productName': 'Tangerine',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 11.99
   },
   {
     'id': 12,
     'assetImageFile': 'watermelon.jpg',
     'productName': 'Watermelon',
-    'productDescription': "It's second great product"
+    'productDescription': "It's second great product",
+    'price': 12.99
   }
 ];
